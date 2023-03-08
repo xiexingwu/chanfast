2fb4b19eb16ecaed73158d47c684e5d87bcb67a3
Michael Xie <michael.xie@rome2rio.com>
2023-03-08 18:41:44 +1100

    adds values to fourth & rk3

---
+++ src/fourth.c
@@ -0,0 +1,6 @@
+#include "fourth.h"
+
+double a1 =  1.125 / 1.0;
+double a2 = -0.125 / 3.0;
+double b1 =  1.125 / 2.0;
+double b2 = -0.125 / 2.0;
---
+++ src/rk3.c
@@ -0,0 +1,6 @@
+#include "rk3.h"
+
+const double rk3_alp[] = { 29.0 /  96.0,  -3.0 / 40.0,  1.0 /  6.0 };
+const double rk3_bet[] = { 37.0 / 160.0,   5.0 / 24.0,  1.0 /  6.0 };
+const double rk3_gam[] = {  8.0 /  15.0,   5.0 / 12.0,  3.0 /  4.0 };
+const double rk3_zet[] = {  0.0        , -17.0 / 60.0, -5.0 / 12.0 };
